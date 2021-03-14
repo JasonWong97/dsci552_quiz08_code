2383388a784ba4f5fc8a5b75aefe1cf8ee272dbf
JasonWong97 <jweng@usc.edu>
2021-03-15 01:09:30 +0800

    first commit

---
+++ q3a.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+# Load the diabetes dataset
+
+dataset=pd.read_csv("data_to_show.csv")
+
+
+
+# Use only one feature
+# data_X = dataset[:, 0]
+data_X = dataset["x1"]
+# data_y = dataset[:, 1]
+data_y = dataset["y"]
+
+print(dataset.shape)
+print(data_X.shape)
+print(data_y.shape)
+
+data1_X=[]
+data1_y=[]
+data2_X=[]
+data2_y=[]
+data3_X=[]
+data3_y=[]
+for i in range(len(data_X)):
+    if data_X[i]*3/2<data_y[i]:
+        data1_X.append(data_X[i])
+        data1_y.append(data_y[i])
+    elif data_X[i]*3/2-150<data_y[i]:
+        # if data_y[i]>150:
+        #     continue
+        data2_X.append(data_X[i])
+        data2_y.append(data_y[i])
+    else:
+        data3_X.append(data_X[i])
+        data3_y.append(data_y[i])
+
+data1_X=np.array(data1_X).reshape(-1,1)
+data1_y=np.array(data1_y).reshape(-1,1)
+data2_X=np.array(data2_X).reshape(-1,1)
+data2_y=np.array(data2_y).reshape(-1,1)
+data3_X=np.array(data3_X).reshape(-1,1)
+data3_y=np.array(data3_y).reshape(-1,1)
+
+size_data=int(len(data_X)*0.2)
+size_data1=int(len(data1_X)*0.2)
+size_data2=int(len(data2_X)*0.2)
+size_data3=int(len(data3_X)*0.2)
+
+
+# Split the data into training/testing sets
+data_X_train = np.array(data_X[:-size_data]).reshape(-1,1)
+data_X_test =  np.array(data_X[-size_data:]).reshape(-1,1)
+
+# Split the targets into training/testing sets
+data_y_train =  np.array(data_y[:-size_data]).reshape(-1,1)
+data_y_test =  np.array(data_y[-size_data:]).reshape(-1,1)
+
+
+data1_X_train = np.array(data1_X[:-size_data1]).reshape(-1,1)
+data1_X_test =  np.array(data1_X[-size_data1:]).reshape(-1,1)
+
+# Split the targets into training/testing sets
+data1_y_train =  np.array(data1_y[:-size_data1]).reshape(-1,1)
+data1_y_test =  np.array(data1_y[-size_data1:]).reshape(-1,1)
+
+
+data2_X_train = np.array(data2_X[:-size_data2]).reshape(-1,1)
+data2_X_test =  np.array(data2_X[-size_data2:]).reshape(-1,1)
+
+# Split the targets into training/testing sets
+data2_y_train =  np.array(data2_y[:-size_data2]).reshape(-1,1)
+data2_y_test =  np.array(data2_y[-size_data2:]).reshape(-1,1)
+
+
+
+
+data3_X_train = np.array(data3_X[:-size_data2]).reshape(-1,1)
+data3_X_test =  np.array(data3_X[-size_data2:]).reshape(-1,1)
+
+# Split the targets into training/testing sets
+data3_y_train =  np.array(data3_y[:-size_data2]).reshape(-1,1)
+data3_y_test =  np.array(data3_y[-size_data2:]).reshape(-1,1)
+
+
+
+
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+regr1 = linear_model.LinearRegression()
+regr2 = linear_model.LinearRegression()
+regr3 = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(data_X_train, data_y_train)
+regr1.fit(data1_X_train, data1_y_train)
+regr2.fit(data2_X_train, data2_y_train)
+regr3.fit(data3_X_train, data3_y_train)
+
+# Make predictions using the testing set
+data_y_pred = regr.predict(data_X_test)
+data_y_pred1 = regr1.predict(data1_X_test)
+data_y_pred2 = regr2.predict(data2_X_test)
+data_y_pred3 = regr3.predict(data3_X_test)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean squared error
+print('Mean squared error: %.2f'
+      % mean_squared_error(data_y_test, data_y_pred))
+# The coefficient of determination: 1 is perfect prediction
+print('Coefficient of determination: %.2f'
+      % r2_score(data_y_test, data_y_pred))
+
+# Plot outputs
+
+
+# p1 = [160, 240] #点p1的坐标值
+# p2 = [40, 160] #点p2的坐标值
+# plt.plot(p1,p2)
+
+# plt.scatter(data_X_test, data_y_test,  color='black')
+
+
+
+
+plt.scatter(data1_X_test, data1_y_test,  color='black')
+plt.plot(data1_X_test, data_y_pred1, color='yellow', linewidth=3)
+
+
+plt.scatter(data2_X_test, data2_y_test,  color='red')
+plt.plot(data2_X_test, data_y_pred2, color='pink', linewidth=3)
+
+plt.scatter(data3_X_test, data3_y_test,  color='orange')
+plt.plot(data3_X_test, data_y_pred3, color='mediumaquamarine', linewidth=3)
+
+
+plt.plot(data_X_test, data_y_pred, color='blue', linewidth=3)
+
+
+plt.xticks(())
+plt.yticks(())
+plt.show()
